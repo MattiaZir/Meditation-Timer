675c7ca59345e26c83dd6ae0a53769b1b1f06ba6
Mattia Zirpoli <Nevermore955@gmail.com>
2017-04-23 19:49:42 +0200

    Changes to main.js

--- app/static/main.js
+++ app/static/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function () {
 
-    $("#add-hours").click({param1: "#hours", param2: true}, controlClickHandler);
-    $("#add-mins").click({param1: "#minutes", param2: true}, controlClickHandler);
-    $("#add-secs").click({param1: "#seconds", param2: true}, controlClickHandler);
-    $("#subtract-hours").click({param1: "#hours", param2: false}, controlClickHandler);
-    $("#subtract-mins").click({param1: "#minutes", param2: false}, controlClickHandler);
-    $("#subtract-secs").click({param1: "#seconds", param2: false}, controlClickHandler);
+    $("#add-hours, #add-minutes, #add-seconds,\
+    #subtract-hours, #subtract-minutes, #subtract-seconds").click(function(event) {
+        controlClickHandler(this);
+    });
 
 });
 
-function controlClickHandler(event) {
-    var element = event.data.param1;
-    var isIncreasing = event.data.param2;
+function controlClickHandler(clickedElement) {
+    /* this regex deletes everything before the character '-' (included)
+    * So it leaves only the element that has to be modified by the method */
+    var element = '#' + clickedElement.id.replace(new RegExp(".+-"), "");
+    /* this regex deletes everything after the character '-' (included)
+    * so that it leaves only the operation that the method needs to perform */
+    var operation = clickedElement.id.replace(new RegExp("-.+"), "");
     var time = $(element).html();
+
+    $(element).html(performOperation(element, time, operation));
+}
+
+function performOperation(element, time, operation) {
     var maxValues = {"#hours": 24, "#minutes": 59, "#seconds": 59};
 
-    if(isIncreasing) {
+    if(operation === "add") {
         if(time < maxValues[element]) {
-            if(element !== "#hours")
-            {
-
-            }
             time++;
         }
     } else {
@@ -37,5 +40,5 @@ function controlClickHandler(event) {
         }
     }
 
-    $(element).html(time);
+    return time;
 }
