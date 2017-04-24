240244309b04d491c680eaa7b00febe1a5f038cf
Mattia Zirpoli <Nevermore955@gmail.com>
2017-04-24 21:53:42 +0200

    Refactoring of main.js

--- app/static/main.js
+++ app/static/main.js
@@ -15,43 +15,64 @@ function controlClickHandler(clickedElement) {
     /* this regex deletes everything after the character '-' (included)
     * so that it leaves only the operation that the method needs to perform */
     var operation = clickedElement.id.replace(new RegExp("-.+"), "");
+
+    (operation === "add") ? performOperation(element, true) : performOperation(element, false);
+}
+function performOperation(element, increasing) {
+    var maxValues = {"#hours": 24, "#minutes": 59, "#seconds": 59};
     var time = $(element).html();
 
-    $(element).html(performOperation(element, time, operation));
-
-
-}
-function performOperation(element, time, operation) {
-    var maxValues = {"#hours": 24, "#minutes": 59, "#seconds": 59};
-
-    if(operation === "add") {
+    if(increasing) {
         if(time < maxValues[element]) {
-            time++;
-        } else {
-
+            time++
         }
     } else {
         if(time > 0) {
-            time--;
+            time--
         }
     }
 
-    if(time < 10) {
-        if(time > 0) {
-            time = '0' + time;
-        } else {
-            time = "00";
-        }
-    }
-
-    return time;
+    $(element).html(pad(time));
+}
+// function to pad the number
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
 }
 function startTimer() {
     var hours = parseInt($("#hours").html()),
         minutes = parseInt($("#minutes").html()),
         seconds = parseInt($("#seconds").html());
-    var date = new Date();
-    date.setHours(hours);
-    date.setMinutes(minutes);
-    date.setSeconds(seconds);
+        milliseconds = (hours * 3600000) + (minutes * 60000) + (seconds * 1000);
+
+        countdown(milliseconds);
+}
+function countdown(milliseconds) {
+    var interval = setInterval(function() {
+        if(milliseconds <= 0) {
+            clearInterval(interval);
+        } else {
+            checkDigits();
+            milliseconds -= 1000
+        }
+    }, 1000);
+}
+
+function checkDigits() {
+    var hoursDiv = $("#hours"),
+        minutesDiv = $("#minutes"),
+        secondsDiv = $("#seconds"),
+        hours = parseInt(hoursDiv.html()),
+        minutes = parseInt(minutesDiv.html()),
+        seconds = parseInt(secondsDiv.html());
+
+    if(seconds === 0 && minutes > 0) {
+        performOperation(minutesDiv, false);
+        secondsDiv.html(59);
+    } else if(seconds === 0 && minutes === 0) {
+        performOperation(hoursDiv, false);
+        minutesDiv.html(59);
+        secondsDiv.html(59);
+    } else {
+        performOperation(secondsDiv, false);
+    }
 }
